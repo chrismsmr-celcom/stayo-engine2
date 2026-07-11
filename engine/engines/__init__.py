20337d58c42fbdb667ab26d70630cc5bf1e7df7f
chrismsmr-celcom <chrismsmr@gmail.com>
2026-07-12 00:16:10 +0100

    Create __init__.py for STAYO Engines module
    Initialize STAYO Engines module with various imports.

---
+++ engine/engines/__init__.py
@@ -0,0 +1,26 @@
+"""
+STAYO Engines - Modules de traitement
+"""
+
+from engine.engines.recommend import recommend
+from engine.engines.intent import understand_intent
+from engine.engines.hotel import fetch_hotels
+from engine.engines.hotel_features import extract_features
+from engine.engines.geo import enrich_distances
+from engine.engines.scoring import score_hotels
+from engine.engines.explain import explain_recommendations
+from engine.engines.activities import suggest_activities
+from engine.engines.traveller import get_profile, save_profile
+
+__all__ = [
+    "recommend",
+    "understand_intent",
+    "fetch_hotels",
+    "extract_features",
+    "enrich_distances",
+    "score_hotels",
+    "explain_recommendations",
+    "suggest_activities",
+    "get_profile",
+    "save_profile"
+]
